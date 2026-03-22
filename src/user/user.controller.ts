08a51a2a10a9c7adca725002780ba0b7fd842344
bluemzzn <synthiawithsyren@gmail.com>
2026-03-22 23:17:37 +0700

    3

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,11 +3,15 @@ import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(readonly userService: UserService) {}
 
-    constructor(readonly userService : UserService){}
-    @Get(':test')
-    test(){
-        return this.userService.test();
+  @Get(':test')
+  test() {
+    return this.userService.test();
+  }
 
-    }
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './user.interface';
+import 'data/users.json';
+import path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class UserService {
-    test(){
-        return [];
-    }
+  test() {
+    return [];
+  }
+
+  findAll(): IUser[] {
+    const filePath = path.join(process.cwd(), 'data/users.json');
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
+  }
 }
